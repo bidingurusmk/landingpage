f50ec62429b39ca2b348597f7c39f730ea1c3e1c
bidingurusmk <bidingurusmk@gmail.com>
2025-11-07 15:01:48 +0700

    rev konten

--- app/berita/[id]/page.tsx
+++ app/berita/[id]/page.tsx
@@ -1,12 +1,16 @@
 
+import {beritaList} from '@/data/berita';
 type Props = {
   id: string
 };
 export default function DetailBerita({params}: {params: Props}) {
   const {id} = params;
+  const berita = beritaList.find((b) => b.id === id);
   return (
     <>
-    selamat datang di detail berita page nilai id: {id}
+    selamat datang di detail berita<br/>
+    judul: {berita?.nama}<br/>
+    isi: {berita?.isi}
     </>   
   );
 }
